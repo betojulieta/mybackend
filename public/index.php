d34120fbe2ceb24201c34733d43131bce4572e95
Humberto Padilla Vega <humpadveg@gmail.com>
2025-04-26 14:07:28 -0600

    Actualización: Configuración CORS corregida

--- public/index.php
+++ public/index.php
@@ -1,5 +1,42 @@
 <?php
 
+// CORS Headers (opcional poner aquí, aunque es mejor hacerlo vía middleware en Laravel)
+header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Capturar la solicitud
+$request = Request::capture();
+
+// Procesar la solicitud
+$response = $app->handle($request);
+
+// Enviar la respuesta
+$response->send();
+
+// Terminar correctamente el ciclo de vida de la solicitud
+$app->terminate($request, $response);
+
+
+
+/*
 header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
@@ -19,7 +56,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+*/

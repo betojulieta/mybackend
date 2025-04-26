3fd3846bcc00e1de10fab83ed54de6c5633b03ab
Humberto Padilla Vega <humpadveg@gmail.com>
2025-04-26 13:00:09 -0600

    Actualización: Configuración CORS corregida

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 

d9179d579b32013a5d7b356c72071b244d6e269f
Humberto Padilla Vega <humpadveg@gmail.com>
2025-04-26 19:39:32 -0600

    Actualización: Configuración CORS corregida

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,27 +1,6 @@
 <?php
 
 
-
-
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php', // Agregamos api.php
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
-
-/*
 use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -53,7 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
 
 
-
+/*
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
--- public/index.php
+++ public/index.php
@@ -3,37 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-
-
-define('LARAVEL_START', microtime(true));
-
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-//$app->handleRequest(Request::capture());
-
-
-
-
-
 // CORS Headers (opcional poner aquí, aunque es mejor hacerlo vía middleware en Laravel)
-//header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
-//header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-//header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-
-/*
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 define('LARAVEL_START', microtime(true));
 
@@ -61,7 +34,7 @@ $response->send();
 // Terminar correctamente el ciclo de vida de la solicitud
 $app->terminate($request, $response);
 
-*/
+
 
 
 

67c8320387e1a6b4f1a473a9a9478422e487818e
Humberto Padilla Vega <humpadveg@gmail.com>
2025-04-26 19:18:55 -0600

    Actualización: Configuración CORS corregida

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
 
-// CORS Headers (opcional poner aquí, aunque es mejor hacerlo vía middleware en Laravel)
-//header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
-//header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-//header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
@@ -22,6 +17,40 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+$app->handleRequest(Request::capture());
+
+
+
+
+
+
+
+
+
+// CORS Headers (opcional poner aquí, aunque es mejor hacerlo vía middleware en Laravel)
+//header('Access-Control-Allow-Origin: https://frontend-rho-liard-56.vercel.app');
+//header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+
+/*
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
 // Capturar la solicitud
 $request = Request::capture();
 
@@ -33,6 +62,10 @@ $response->send();
 
 // Terminar correctamente el ciclo de vida de la solicitud
 $app->terminate($request, $response);
+
+*/
+
+
 
 
 
